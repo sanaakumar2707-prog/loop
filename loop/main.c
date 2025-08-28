38adf077754958c007ab537bb83c73488f4fe081
sanaa <Sanaakumar07@gmail.com>
2025-08-28 10:38:21 +0530

    Initial Commit

---
+++ loop/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  loop
+//
+//  Created by Sanaa Kumar on 28/08/25.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
